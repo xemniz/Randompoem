677279a7ccdb197537a80bb81f0d263132226374
Nikolay Konorev <sglvzom@gmail.com>
2017-07-14 01:11:19 +0300

    random size fix

--- app/src/main/java/ru/xmn/randompoem/screens/RandomPoemsViewModel.kt
+++ app/src/main/java/ru/xmn/randompoem/screens/RandomPoemsViewModel.kt
@@ -64,7 +64,7 @@ class RandomPoemsInteractor @Inject constructor(val poemsRepository: PoemsReposi
 
 public fun <T> List<T>.randomItem(): T {
     val r = Random()
-    return this[r.nextInt(this.size - 1)]
+    return if (this.size - 1 == 0) this[r.nextInt(this.size - 1)] else this[0]
 }
 
 @Module
